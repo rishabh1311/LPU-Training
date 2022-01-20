4a913e26a9320e6da5b9a488d283548caca6dfa9
rishabh1311 <73522454+rishabh1311@users.noreply.github.com>
2022-01-20 23:20:47 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,14 @@
+
+public class Assignment {
+
+	public static void main(String[] args) {
+
+	    for (int i = 1; i <= 8; i++) {
+
+	      if (i>3 && i<7) {
+	        continue;
+	      }
+	      System.out.println(i);
+	    }
+	  }
+}
