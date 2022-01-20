47831d7d63fab281dbf978d71b6686912601423d
rishabh1311 <73522454+rishabh1311@users.noreply.github.com>
2022-01-20 23:32:56 +0530

    Add files via upload

---
+++ lecture1.java
@@ -0,0 +1,17 @@
+package Rishabh;
+
+public class lecture1 {
+
+	public static void main(String[] args) {
+				// Simply Code here in the main
+				System.out.println("We are learning java. stick to the concepts well");
+				System.out.println("This is all for the very 1st session");
+				System.out.println("Thank You. I hope you all enjoyed");
+
+			}
+
+		}
+
+class One{
+			
+}
